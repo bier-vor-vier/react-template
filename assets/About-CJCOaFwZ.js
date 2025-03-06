2c69e260d8a6996501b1fe89bc9d3681ea60da0c
bender0oo0 <155059128+bender0oo0@users.noreply.github.com>
2025-03-06 21:09:41 +0000

    Deploying to gh-pages from @ bier-vor-vier/react-template@aa0ff5450063382c4e354546e8ce2c55a966a449 🚀

---
+++ assets/About-CJCOaFwZ.js
@@ -0,0 +1,1 @@
+import{j as s}from"./react-BECzdS4g.js";const o=()=>s.jsx("h1",{className:"p-4",children:"ℹ️ About"});export{o as default};
--- assets/Home-B6omnv3K.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as e}from"./index-Cyfu9DX1.js";const a=()=>{const{t:s}=t();return e.jsx("h1",{className:"p-4",children:s("home")})};export{a as default};
